7afcd29deddddf491093fd9871c7a081b04467f7
Muhammad Hassan <>
2025-04-16 21:30:44 -0400

    added demo project

---
+++ SkeletonDemo/SkeletonDemo/SkeletonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkeletonDemoApp.swift
+//  SkeletonDemo
+//
+//  Created by Muhammad Hassan on 2025-04-16.
+//
+
+import SwiftUI
+
+@main
+struct SkeletonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
